63bce85e6542a641bc2c8ee9445de489dad687c9
ivaaane <ivanzaragoza2007@gmail.com>
2024-06-22 14:55:50 +0200

    Added tons of sh*t I don't remember anymore sry

--- main.lua
+++ main.lua
@@ -1,32 +1,23 @@
 -- Start title
 local printTitle = function ()
     print [[
-    $$\
-    $$ |
-    $$$$$$$\   $$$$$$\  $$$$$$$\   $$$$$$\  $$$$$$\$$$$\   $$$$$$\  $$$$$$$\
-    $$  __$$\  \____$$\ $$  __$$\ $$  __$$\ $$  _$$  _$$\  \____$$\ $$  __$$\
-    $$ |  $$ | $$$$$$$ |$$ |  $$ |$$ /  $$ |$$ / $$ / $$ | $$$$$$$ |$$ |  $$ |
-    $$ |  $$ |$$  __$$ |$$ |  $$ |$$ |  $$ |$$ | $$ | $$ |$$  __$$ |$$ |  $$ |
-    $$ |  $$ |\$$$$$$$ |$$ |  $$ |\$$$$$$$ |$$ | $$ | $$ |\$$$$$$$ |$$ |  $$ |
-    \__|  \__| \_______|\__|  \__| \____$$ |\__| \__| \__| \_______|\__|  \__|
-                                $$\   $$ |
-                                \$$$$$$  |
-                                \______/
+$$\
+$$ |
+$$$$$$$\   $$$$$$\  $$$$$$$\   $$$$$$\  $$$$$$\$$$$\   $$$$$$\  $$$$$$$\
+$$  __$$\  \____$$\ $$  __$$\ $$  __$$\ $$  _$$  _$$\  \____$$\ $$  __$$\
+$$ |  $$ | $$$$$$$ |$$ |  $$ |$$ /  $$ |$$ / $$ / $$ | $$$$$$$ |$$ |  $$ |
+$$ |  $$ |$$  __$$ |$$ |  $$ |$$ |  $$ |$$ | $$ | $$ |$$  __$$ |$$ |  $$ |
+$$ |  $$ |\$$$$$$$ |$$ |  $$ |\$$$$$$$ |$$ | $$ | $$ |\$$$$$$$ |$$ |  $$ |
+\__|  \__| \_______|\__|  \__| \____$$ |\__| \__| \__| \_______|\__|  \__|
+                            $$\   $$ |
+                            \$$$$$$  |
+                             \______/
+By Ivan :3
 
-    Welcome to Lua Hangman by Iván!
-    ===============================
+===============================
 
     ]]
 end
-
--- Initial setup
-local wordList = require "words"
-local randWord = wordList[math.random(1, #wordList)]
-local word = {}
-for i = 1, #randWord do
-    word[i] = string.sub(randWord, i, i)
-end
-local art = require "art"
 
 -- Function to check if a table contains a value
 local valueInTable = function (table, value)
@@ -38,20 +29,73 @@ local valueInTable = function (table, value)
     return false
 end
 
--- Game loop
-printTitle()
+-- Initial setup
+local art = require "art"
+local wordList = require "words"
+local randWord = wordList[math.random(1, #wordList)]
+local word = {}
+for i = 1, #randWord do word[i] = string.sub(randWord, i, i) end
+local errors = 0
+local guesses = {}
+local actionText = "Introduce your guess."
 local playing = true
-while playing do
-    print (art[1])
-    print "Introduce your guess."
-    local input = io.read()
-    if input and string.len(input) == 1 then
-        if valueInTable(word, input) then
-            print (input .. " is included!")
+printTitle()
+
+-- Function to print information
+local printInformation = function ()
+    local displayText = "" -- Display the complete word with already guessed letters
+    for _,v in pairs(word) do
+        if valueInTable(guesses, v) then
+            displayText = displayText .. string.upper(v) .. " "
         else
-            print (input .. " is not included.")
+            displayText = displayText .. "_ "
+        end
+    end
+
+    local saidLetters = "" -- Displays the said letters
+    for _,v in pairs({
+        "a","b","c","d","e","f","g",
+        "h","i","j","k","l","m","n",
+        "o","p","q","r","s","t","u",
+        "v","w","x,","y","z"
+    }) do
+        if valueInTable(guesses, v) then
+            saidLetters = saidLetters .. v .. " "
+        end
+    end
+
+    print(art[errors + 1])
+    print (displayText .. "\n")
+    print ("Errors: " .. errors)
+    print ("Letters said: " .. saidLetters)
+    print ("\n>> " .. actionText .. "\n")
+end
+
+-- GAME LOOP --
+while playing do
+    printInformation()
+
+    -- Check input
+    local input = string.lower(io.read())
+    print (">> " .. input)
+
+    print "\n===============================\n"
+
+    if input and string.len(input) == 1 then
+        if not valueInTable(guesses, input) then
+
+            table.insert(guesses, input)
+            if valueInTable(word, input) then
+                actionText = "\""..input.."\" is included!"
+            else
+                errors = errors + 1
+                actionText = "\""..input.."\" is not included"
+            end
+
+        else
+            print (">> You already said that letter!")
         end
     else
-        print ("There was an error with the input. Try again.")
+        print (">> There was an error with the input. Try again.\n")
     end
 end
